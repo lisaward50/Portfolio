e08962e0a30f5bec2b0c65855d41ba2facaf1459
lisaward50 <lisa.ward50@gmail.com>
2018-11-25 17:27:04 -0500

    Updates

---
+++ precache-manifest.b9dbafff3ed52a0a5bf186bf210e5250.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6457b95fa3e28b9b6b41",
+    "url": "/Portfolio/static/css/main.daaf910b.chunk.css"
+  },
+  {
+    "revision": "6457b95fa3e28b9b6b41",
+    "url": "/Portfolio/static/js/main.6457b95f.chunk.js"
+  },
+  {
+    "revision": "0cd6058fc3c24877ba89",
+    "url": "/Portfolio/static/js/1.0cd6058f.chunk.js"
+  },
+  {
+    "revision": "3b96116072eb85f5a3a2",
+    "url": "/Portfolio/static/js/runtime~main.3b961160.js"
+  },
+  {
+    "revision": "4db42e7ff814eb7bd1de5f88a75fdc3b",
+    "url": "/Portfolio/static/media/brick.4db42e7f.jpg"
+  },
+  {
+    "revision": "8880429b311b3870b791899b6f651ef1",
+    "url": "/Portfolio/index.html"
+  }
+];
